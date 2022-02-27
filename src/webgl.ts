ca66d23a3d018e4206d81b81eb669f487b705989
Jeff Schofield <jeff@jeffschofield.com>
2022-02-27 11:48:20 -0500

    feat: Add program creation helper

--- src/webgl.ts
+++ src/webgl.ts
@@ -1,2 +1,13 @@
 export function createProgram(gl:WebGL2RenderingContext, vertex_shader:WebGLShader, fragment_shader:WebGLShader) {
+	let program = gl.createProgram()
+	if (!program) throw new Error('Unable to create program')
+
+	gl.attachShader(program, vertex_shader)
+	gl.attachShader(program, fragment_shader)
+	gl.linkProgram(program)
+
+	let success = gl.getProgramParameter(program, gl.LINK_STATUS)
+	if (!success) throw new Error('Unable to create program')
+
+	return program
 }
